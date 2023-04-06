98d73bb4f64b282b90da20b0ece1563072b3522f
Joaquin Flores <49995581+joaflores03@users.noreply.github.com>
2023-04-06 01:07:10 -0300

    Add files via upload

---
+++ Pomodoro.py
@@ -0,0 +1,17 @@
+def inicioTarea():
+    tarea = input("Ingresa la tarea a realizar: ")
+    print(f"La tarea a realizar es {tarea}")
+
+def menu():
+    print("""
+    Seleccione alguna de las siguientes cantidades de tiempo:
+    1)10 Minutos
+    2)25 Minutos
+    3)45 Minutos
+    4)1 Hora
+    0)Salir
+    """)
+
+def inicioPomodoro():
+    opc = input() 
+    
